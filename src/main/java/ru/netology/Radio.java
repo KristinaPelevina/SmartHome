484448333e485e209052ce82f934a65bc56904c4
Kristina Pelevina <christya_13.07@mail.ru>
2020-08-11 14:59:04 +0600

    Update Radio.java class with Lombok designers. Removed manual getters and setters

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -19,29 +19,17 @@ public class Radio {
     private int maxVolume = 100;
     private boolean on;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getCurrentStation() {
-        return currentStation;
-    }
-
     public void setCurrentStation(int currentStation) {
-        if (currentStation > maxStation){
+        if (currentStation > maxStation) {
             return;
         }
-        if (currentStation < minStation){
+        if (currentStation < minStation) {
             return;
         }
-         this.currentStation = currentStation;
+        this.currentStation = currentStation;
     }
 
-    public void previousStation () {
+    public void previousStation() {
         int previousStation = currentStation - 1;
         if (previousStation < minStation) {
             this.currentStation = maxStation;
@@ -50,41 +38,13 @@ public class Radio {
         }
     }
 
-    public void nextStation () {
+    public void nextStation() {
         int nextStation = currentStation + 1;
         if (nextStation > maxStation) {
             this.currentStation = minStation;
         } else {
             this.currentStation = nextStation;
         }
-    }
-
-    public int getDefaultStation() {
-        return defaultStation;
-    }
-
-    public void setDefaultStation(int defaultStation) {
-        this.defaultStation = defaultStation;
-    }
-
-    public int getMinStation() {
-        return minStation;
-    }
-
-    public void setMinStation(int minStation) {
-        this.minStation = minStation;
-    }
-
-    public int getMaxStation() {
-        return maxStation;
-    }
-
-    public void setMaxStation(int maxStation) {
-        this.maxStation = maxStation;
-    }
-
-    public int getCurrentVolume() {
-        return currentVolume;
     }
 
     public void setCurrentVolume(int currentVolume) {
@@ -97,7 +57,7 @@ public class Radio {
         this.currentVolume = currentVolume;
     }
 
-    public void increasedCurrentVolume () {
+    public void increasedCurrentVolume() {
         int nextVolume = currentVolume + 1;
         if (nextVolume > maxVolume) {
             return;
@@ -105,35 +65,12 @@ public class Radio {
         this.currentVolume = nextVolume;
     }
 
-    public void decreasedCurrentVolume () {
+    public void decreasedCurrentVolume() {
         int previousVolume = currentVolume - 1;
         if (previousVolume < minVolume) {
-           return;
+            return;
         }
         this.currentVolume = previousVolume;
     }
-
-    public int getMinVolume() {
-        return minVolume;
-    }
-
-    public void setMinVolume(int minVolume) {
-        this.minVolume = minVolume;
-    }
-
-    public int getMaxVolume() {
-        return maxVolume;
-    }
-
-    public void setMaxVolume(int maxVolume) {
-        this.maxVolume = maxVolume;
-    }
-
-    public boolean isOn() {
-        return on;
-    }
-
-    public void setOn(boolean on) {
-        this.on = on;
-    }
 }
+
